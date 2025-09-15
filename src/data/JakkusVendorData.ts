ed9ca703735ae102ffe501cce3adf01622c05df5
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-14 18:03:06 -0700

    Added a ts file for Jakkus Vendor Data

---
+++ src/data/JakkusVendorData.ts
@@ -0,0 +1,94 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Mount";
+   bronzeCost: number;
+}
+
+export const JakkusVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Reins of the Highlord's Felscorned Charger",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 2,
+      itemName: "Iron Reigns of the Felscorned War Wrym",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 3,
+      itemName: "Felscorned Reins of the Vilebrood Vanquisher",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 4,
+      itemName: "Reigns of the Felscorned Wolfhawk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 5,
+      itemName: "Felscorned Tempest Totem",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 6,
+      itemName: "Shadowblade's Reins of the Felscorned Portent",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 7,
+      itemName: "Felscorned Grandmaster's Bell",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 8,
+      itemName: "Feldruid's Scornwing Idol",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 9,
+      itemName: "Slayer's Reins of the Felscorned Shrieker",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 10,
+      itemName: "Felscorned Seeker's Whistle",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 11,
+      itemName: "Felscorned Reins of the Netherlord's Dreadsteed",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 12,
+      itemName: "Archmage's Felscorned Disc",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   }
+]
