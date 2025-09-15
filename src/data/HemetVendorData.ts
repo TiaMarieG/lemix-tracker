7f8921f7236f1679f8d650559a8d9a1272efcb60
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-14 18:54:52 -0700

    Added a ts file for Hemet Vendor Data

---
+++ src/data/HemetVendorData.ts
@@ -0,0 +1,430 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Mount";
+   bronzeCost: number;
+}
+
+export const HemetVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Reins of the Illidari Dreadstalker",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 2,
+      itemName: "Reins of the Illidari Blightsalker",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 3,
+      itemName: "Reins of the Highland Elderhorn",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 4,
+      itemName: "Reins of the Treetop Highmountain Eagle",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 5,
+      itemName: "Reins of the Snowy Highmountain Eagle",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 6,
+      itemName: "Reins of the Wretched Fel Bat",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 7,
+      itemName: "Reins of the Ashplague Fel Bat",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 8,
+      itemName: "Reins of the Bloodhunter Fel Bat",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 9,
+      itemName: "Reins of the Gloomdark Nightmare",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 10,
+      itemName: "Reins of the Turquoise Courser",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 11,
+      itemName: "Reins of the Golden Sunrunner",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 12,
+      itemName: "Reins of the Twilight Courser",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 13,
+      itemName: "Reins of the Brimstone Courser",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 14,
+      itemName: "Reins of the Chestnut Courser",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 15,
+      itemName: "Reins of the Leystone Basilisk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 16,
+      itemName: "Reins of the Felslate Basilisk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 17,
+      itemName: "Reins of the Slag Basilisk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 18,
+      itemName: "Reins of the Aquamarine Basilisk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 19,
+      itemName: "Reins of the Long-Forgotten Hippogryph",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 20,
+      itemName: "Spirit of Eche'ro",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 21,
+      itemName: "Arcadian War Turtle",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   },
+   {
+      id: 22,
+      itemName: "Reins of the Arcberry Manasaber",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 23,
+      itemName: "Reins of the Bonesteed of Triumph",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 24,
+      itemName: "Reins of the Bonesteed of Bloodshed",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 25,
+      itemName: "Reins of the Bonesteed of Plague",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 26,
+      itemName: "Reins of the Bonesteed of Oblivion",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 27,
+      itemName: "Midnight's Eternal Reins",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   },
+   {
+      id: 28,
+      itemName: "Living Infernal Core",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   },
+   {
+      id: 29,
+      itemName: "Fiendish Hellfire Core",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   },
+   {
+      id: 30,
+      itemName: "Cloudwing Hippogryph",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 31,
+      itemName: "Highmountain Elderhorn",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 32,
+      itemName: "Leywoven Flying Carpet",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 33,
+      itemName: "Valarjar Stormwing",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 34,
+      itemName: "Abyss Worm",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   },
+   {
+      id: 35,
+      itemName: "Wild Dreamrunner",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 36,
+      itemName: "Reins of the Ghastly Ur'zul",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 37,
+      itemName: "Reins of the Luminous Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 38,
+      itemName: "Reins of the Albino Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 39,
+      itemName: "Reins of the Bloodtooth Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 40,
+      itemName: "Reins of the Fel-Scarred Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 41,
+      itemName: "Garnet Ruinstrider",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 42,
+      itemName: "Reins of the Longhorned Beryl Talbuk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 43,
+      itemName: "Reins of the Longhorned Argussian Talbuk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 44,
+      itemName: "Longhorned Bleakhoof Talbuk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 45,
+      itemName: "Reins of the Longhorned Sable Talbuk",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 10000
+   },
+   {
+      id: 46,
+      itemName: "Antoran Charhound",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   },
+   {
+      id: 47,
+      itemName: "Biletooth Gnasher",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 48,
+      itemName: "Acid Belcher",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 49,
+      itemName: "Crimson Slavermaw",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 50,
+      itemName: "Blessed Felcrusher",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 51,
+      itemName: "Avenging Felcrusher",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 52,
+      itemName: "Glorious Felcrusher",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 20000
+   },
+   {
+      id: 53,
+      itemName: "Vile Fiend",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 54,
+      itemName: "Darkspore Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 55,
+      itemName: "Lambent Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 56,
+      itemName: "Felglow Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 57,
+      itemName: "Scintillating Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 58,
+      itemName: "Vibrant Mana Ray",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 59,
+      itemName: "Maddened Chaosrunner",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 40000
+   },
+   {
+      id: 60,
+      itemName: "Shackled Ur'zul",
+      itemUrl: "N/A",
+      category: "Mount",
+      bronzeCost: 100000
+   }
+]
