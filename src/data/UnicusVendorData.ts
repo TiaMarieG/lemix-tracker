584d6d66bcdbfb52cbdc7cb714c929302aa477d1
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-15 09:03:20 -0700

    Added a ts file for Unicus Vendor Data

---
+++ src/data/UnicusVendorData.ts
@@ -0,0 +1,297 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Arsenal" | "Ensemble";
+   bronzeCost: number;
+}
+
+export const UnicusVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Arsenal: Arms of the Felforged Knight",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 2,
+      itemName: "Arsenal: Odyn's Spears",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 3,
+      itemName: "Ensemble: Windrunner Quivers",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 4,
+      itemName: "Ensemble: Blazing Dreamscribed Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 5,
+      itemName: "Ensemble: Dreamwatcher Vestments",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 6,
+      itemName: "Ensemble: Dreamseeker Vestments",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 7,
+      itemName: "Ensemble: Nightrune Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 8,
+      itemName: "Ensemble: Earthrune Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 9,
+      itemName: "Ensemble: Skyrune Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 10,
+      itemName: "Ensemble: Slayer's Golden Scarguards",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 11,
+      itemName: "Ensemble: Slayer's Silver Scarguards",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 12,
+      itemName: "Ensemble: Fel-Bloodied Battlegear",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 13,
+      itemName: "Ensemble: Searaider's Battlegarb",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 14,
+      itemName: "Ensemble: Gladeraider's Battlegarb",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 15,
+      itemName: "Ensemble: Jarl's Battlescales",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 16,
+      itemName: "Ensemble: Ruby Drake Hunter's Kit",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 17,
+      itemName: "Ensemble: Highpeak Dragonscale",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 18,
+      itemName: "Ensemble: Dreamweald Dragonscale",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 19,
+      itemName: "Ensemble: Firewurm Dragonscale",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 20,
+      itemName: "Ensemble: Jarl's Battlehorns",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 21,
+      itemName: "Ensemble: Storm Champion's Warharness",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 22,
+      itemName: "Ensemble: Argussian Demonsbane Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 23,
+      itemName: "Ensemble: Vestments of Eredathian Sacrifice",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 24,
+      itemName: "Ensemble: Antoran Guard's Golden Battleplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 25,
+      itemName: "Ensemble: Heritage of the Lightforged - Holy Gold",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 26,
+      itemName: "Ensemble: Heritage of the Lightforged - Holy Gold",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 27,
+      itemName: "Ensemble: Heritage of the Lightforged - Crimson Vengeance",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 28,
+      itemName: "Ensemble: Heritage of the Shal'dorei - Vineyard Red",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 29,
+      itemName: "Ensemble: Stygian Silks",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 30,
+      itemName: "Ensemble: Dream Defender's Emerald Guardplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 31,
+      itemName: "Ensemble: Barkbinds of the Archdruid's Nightmare",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 32,
+      itemName: "Ensemble: Fel-Marked Scales",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 33,
+      itemName: "Ensemble: Verdant Dreamscribed Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 34,
+      itemName: "Ensemble: Tidesoaked Battlegear",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 35,
+      itemName: "Ensemble: Mantles of the Nightwell",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 36,
+      itemName: "Ensemble: Tideskorn Hunter's Munitions",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 7500
+   },
+   {
+      id: 37,
+      itemName: "Arsenal: Mo'arg Swords",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 38,
+      itemName: "Arsenal: Bone Scythes",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 39,
+      itemName: "Arsenal: Immortal Maces",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 40,
+      itemName: "Arsenal: Mo'arg Hornmaces",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   },
+   {
+      id: 41,
+      itemName: "Arsenal: Gems of the Lightforged Draenei",
+      itemUrl: "N/A",
+      category: "Arsenal",
+      bronzeCost: 7500
+   }
+]
