ceb0b05835861d16f94eb781dba8291fed7c0c6b
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-15 09:38:49 -0700

    Added a ts file for Larah Vendor Data

---
+++ src/data/LarahVendorData.ts
@@ -0,0 +1,346 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Ensemble";
+   bronzeCost: number;
+}
+
+export const LarahVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Ensemble: Fel-Infused Cloth Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 2,
+      itemName: "Ensemble: Felshroud Leather Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 3,
+      itemName: "Ensemble: Fel-Chain Mail Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 4,
+      itemName: "Ensemble: Felforged Plate Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 5,
+      itemName: "Ensemble: Vestments of the Manasinged",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 6,
+      itemName: "Ensemble: Moonfall Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 7,
+      itemName: "Ensemble: Battlegear of the Dreadhide Stalker",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 8,
+      itemName: "Ensemble: Ambervale Bonehide",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 9,
+      itemName: "Ensemble: Chains of Helheim",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 10,
+      itemName: "Ensemble: Darkwatcher Bindings",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 11,
+      itemName: "Ensemble: Jandvik Diver's Metal",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 12,
+      itemName: "Ensemble: Leyline Defender's Sunplate Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 13,
+      itemName: "Ensemble: Regalia of the Hrydshal Runespeaker",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 14,
+      itemName: "Ensemble: Crescent Vale Raiment",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 15,
+      itemName: "Ensemble: Wine-dark Royal Robes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 16,
+      itemName: "nsemble: Leyline Scholar's Regalia",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 17,
+      itemName: "Ensemble: Highmountain Hides",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 18,
+      itemName: "Ensemble: Haustvelt Leathers",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 19,
+      itemName: "Ensemble: Sablehide Vestments",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 20,
+      itemName: "Ensemble: Llothien Prowler's Kit",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 21,
+      itemName: "Ensemble: Scavenged Chains of Karazhan",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 22,
+      itemName: "Ensemble: Dreadthorn Battlegear",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 23,
+      itemName: "Ensemble: Scales of Remembered Eternity",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 24,
+      itemName: "Ensemble: Stormborn Laminar Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 25,
+      itemName: "Ensemble: Highmountain Riverscales",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 26,
+      itemName: "Ensemble: Thunderpeak Boneguards",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 27,
+      itemName: "Ensemble: Nar'thalas Graduate's Trim",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 28,
+      itemName: "Ensemble: Kal'delar Battleplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 29,
+      itemName: "Ensemble: Vrykul Funereal Regalia",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 30,
+      itemName: "Ensemble: Riven Priesthood Regalia",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 31,
+      itemName: "Ensemble: Lunarblight Leathers",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 32,
+      itemName: "Ensemble: Shrinebreaker's Battlegear",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 33,
+      itemName: "Ensemble: Moonshatter Warplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 34,
+      itemName: "Ensemble: Vileweave Vestments",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 35,
+      itemName: "Ensemble: Netherfiend Battlegear",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 36,
+      itemName: "Ensemble: Ered'ruin Scalemail",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 37,
+      itemName: "Ensemble: Xorothian Plate Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 38,
+      itemName: "Ensemble: Garothi Battleplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 39,
+      itemName: "Ensemble: Doomsinger's Cloth Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 40,
+      itemName: "Ensemble: Arinor Keeper's Leather Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 41,
+      itemName: "Ensemble: Oronaar Disciple's Mail Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 42,
+      itemName: "Ensemble: Praetorium Guard's Plate Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 43,
+      itemName: "Ensemble: Stygian Hides",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 44,
+      itemName: "Ensemble: Zealous Felslingers Battle Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 45,
+      itemName: "Ensemble: World-Defiler's Battle Armor",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 46,
+      itemName: "Ensemble: Forgotten Conservatory Clothes",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 47,
+      itemName: "Ensemble: Eredath Lightseeker's Regalia",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 48,
+      itemName: "Ensemble: Triumvirate High Guard's Battlegear",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   }
+]
