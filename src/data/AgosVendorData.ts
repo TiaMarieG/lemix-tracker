fdc0d76e62bf4383d3035298afacae92938a0a46
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-15 16:58:55 -0700

    Added a ts file for Agos Vendor Data

---
+++ src/data/AgosVendorData.ts
@@ -0,0 +1,59 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Ensemble";
+   bronzeCost: number;
+}
+
+export const AgosVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Ensemble: Skyborne Brigandine",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 2,
+      itemName: "Ensemble: Seaborn Brigandine",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 3,
+      itemName: "Ensemble: Sunborne Runemail",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 4,
+      itemName: "Ensemble: Earthbreaker Dragonscale",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 5,
+      itemName: "Ensemble: Val'kyr's Warharness",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 6,
+      itemName: "Ensemble: Winged Plate of the Valhalas Champion",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 7,
+      itemName: "Ensemble: Emerald Drake Hunter's Kit",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   }
+]
