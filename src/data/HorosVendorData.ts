a7a12dea98bbba8f9676ac1833a30f4bcf70f2d3
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-14 17:34:26 -0700

    Installed MUI, created home page, created .ts for Horus Vendor Data

---
+++ src/data/HorosVendorData.ts
@@ -0,0 +1,367 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Mount" | "Pet" | "Toy" | "Transmog" | "Quest Item";
+   bronzeCost: number;
+}
+
+export const HorosVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Morsel",
+      itemUrl: "https://www.wowhead.com/ptr/item=239705/morsel",
+      category: "Pet",
+      bronzeCost: 5000
+   },
+   {
+      id: 2,
+      itemName: "Tidbit",
+      itemUrl: "https://www.wowhead.com/ptr/item=239699/tidbit",
+      category: "Pet",
+      bronzeCost: 5000
+   },
+   {
+      id: 3,
+      itemName: "Son of Goredome",
+      itemUrl: "https://www.wowhead.com/ptr/item=129108/son-of-goredome",
+      category: "Pet",
+      bronzeCost: 5000
+   },
+   {
+      id: 4,
+      itemName: "Strange Humming Crystal",
+      itemUrl: "https://www.wowhead.com/item=141349/strange-humming-crystal",
+      category: "Quest Item",
+      bronzeCost: 100000
+   },
+   {
+      id: 5,
+      itemName: "Corgnelius",
+      itemUrl: "https://www.wowhead.com/item=140320/corgnelius",
+      category: "Pet",
+      bronzeCost: 10000
+   },
+   {
+      id: 6,
+      itemName: "Eye of Inquisition",
+      itemUrl: "https://www.wowhead.com/item=136901/eye-of-inquisition",
+      category: "Pet",
+      bronzeCost: 100000
+   },
+   {
+      id: 7,
+      itemName: "Firebat Pup",
+      itemUrl: "https://www.wowhead.com/item=140316/firebat-pup",
+      category: "Pet",
+      bronzeCost: 10000
+   },
+   {
+      id: 8,
+      itemName: "Hateful Eye",
+      itemUrl: "https://www.wowhead.com/item=136900/hateful-eye",
+      category: "Pet",
+      bronzeCost: 35000
+   },
+   {
+      id: 9,
+      itemName: "Nightmare Whelpling",
+      itemUrl: "https://www.wowhead.com/item=136903/nightmare-whelpling",
+      category: "Pet",
+      bronzeCost: 80000
+   },
+   {
+      id: 10,
+      itemName: "Wyrmy Tunkins",
+      itemUrl: "https://www.wowhead.com/item=136922/wyrmy-tunkins",
+      category: "Pet",
+      bronzeCost: 10000
+   },
+   {
+      id: 11,
+      itemName: "Thistleleaf Adventurer",
+      itemUrl: "https://www.wowhead.com/item=130167/thistleleaf-adventurer",
+      category: "Pet",
+      bronzeCost: 100000
+   },
+   {
+      id: 12,
+      itemName: "Rebellious Imp",
+      itemUrl: "https://www.wowhead.com/item=153252/rebellious-imp",
+      category: "Pet",
+      bronzeCost: 100000
+   },
+   {
+      id: 13,
+      itemName: "Crystalline Eye of Undravius",
+      itemUrl: "https://www.wowhead.com/item=131724/crystalline-eye-of-undravius",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 14,
+      itemName: "Starlight Beacon",
+      itemUrl: "https://www.wowhead.com/item=131717/starlight-beacon",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 15,
+      itemName: "Barnacle-Encrusted Gem",
+      itemUrl: "https://www.wowhead.com/item=129165/barnacle-encrusted-gem",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 16,
+      itemName: "Tournament Favor",
+      itemUrl: "https://www.wowhead.com/item=130169/tournament-favor",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 17,
+      itemName: "Pocket Fel Spreader",
+      itemUrl: "https://www.wowhead.com/item=140363/pocket-fel-spreader",
+      category: "Toy",
+      bronzeCost: 20000
+   },
+   {
+      id: 18,
+      itemName: "Mote of Light",
+      itemUrl: "https://www.wowhead.com/item=141862/mote-of-light",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 19,
+      itemName: "Stormforged Vrykul Horn",
+      itemUrl: "https://www.wowhead.com/item=140160/stormforged-vrykul-horn",
+      category: "Toy",
+      bronzeCost: 80000
+   },
+   {
+      id: 20,
+      itemName: "Tylarr Gronnden",
+      itemUrl: "https://www.wowhead.com/item=144394/tylarr-gronnden",
+      category: "Pet",
+      bronzeCost: 35000
+   },
+   {
+      id: 21,
+      itemName: "Big Red Raygun",
+      itemUrl: "https://www.wowhead.com/item=142265/big-red-raygun",
+      category: "Toy",
+      bronzeCost: 35000
+   },
+   {
+      id: 22,
+      itemName: "Crate of Bobbers: Tugboat",
+      itemUrl: "https://www.wowhead.com/item=142530/crate-of-bobbers-tugboat",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 23,
+      itemName: "Crate of Bobbers: Cat Head",
+      itemUrl: "https://www.wowhead.com/item=142529/crate-of-bobbers-cat-head",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 24,
+      itemName: "Crate of Bobbers: Can of Worms",
+      itemUrl: "https://www.wowhead.com/item=142528/crate-of-bobbers-can-of-worms",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 25,
+      itemName: "Crate of Bobbers: Wooden Pepe",
+      itemUrl: "https://www.wowhead.com/item=143662/crate-of-bobbers-wooden-pepe",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 26,
+      itemName: "Golden Hearthstone Card: Lord Jaraxxus",
+      itemUrl: "https://www.wowhead.com/item=119211/golden-hearthstone-card-lord-jaraxxus",
+      category: "Toy",
+      bronzeCost: 100000
+   },
+   {
+      id: 27,
+      itemName: "Scraps",
+      itemUrl: "https://www.wowhead.com/item=146953/scraps",
+      category: "Pet",
+      bronzeCost: 80000
+   },
+   {
+      id: 28,
+      itemName: "Orphaned Felbat",
+      itemUrl: "https://www.wowhead.com/item=147841/orphaned-felbat",
+      category: "Pet",
+      bronzeCost: 35000
+   },
+   {
+      id: 29,
+      itemName: "Ageless Bronze Drake",
+      itemUrl: "https://www.wowhead.com/item=151828/ageless-bronze-drake",
+      category: "Pet",
+      bronzeCost: 80000
+   },
+   {
+      id: 30,
+      itemName: "Bronze Proto-Whelp",
+      itemUrl: "https://www.wowhead.com/item=151829/bronze-proto-whelp",
+      category: "Pet",
+      bronzeCost: 80000
+   },
+   {
+      id: 31,
+      itemName: "Sira's Extra Cloak",
+      itemUrl: "https://www.wowhead.com/item=147843/siras-extra-cloak",
+      category: "Toy",
+      bronzeCost: 35000
+   },
+   {
+      id: 32,
+      itemName: "Pilfered Sweeper",
+      itemUrl: "https://www.wowhead.com/item=147867/pilfered-sweeper",
+      category: "Toy",
+      bronzeCost: 35000
+   },
+   {
+      id: 33,
+      itemName: "Uuna's Doll",
+      itemUrl: "https://www.wowhead.com/item=153195/uunas-doll",
+      category: "Pet",
+      bronzeCost: 10000
+   },
+   {
+      id: 34,
+      itemName: "Fel-Afflicted Skyfin",
+      itemUrl: "https://www.wowhead.com/item=153055/fel-afflicted-skyfin",
+      category: "Pet",
+      bronzeCost: 20000
+   },
+   {
+      id: 35,
+      itemName: "Docile Skyfin",
+      itemUrl: "https://www.wowhead.com/item=153054/docile-skyfin",
+      category: "Pet",
+      bronzeCost: 20000
+   },
+   {
+      id: 36,
+      itemName: "Cross Gazer",
+      itemUrl: "https://www.wowhead.com/item=153026/cross-gazer",
+      category: "Pet",
+      bronzeCost: 10000
+   },
+   {
+      id: 37,
+      itemName: "Grasping Manifestation",
+      itemUrl: "https://www.wowhead.com/item=153056/grasping-manifestation",
+      category: "Pet",
+      bronzeCost: 20000
+   },
+   {
+      id: 38,
+      itemName: "All-Seer's Eye",
+      itemUrl: "https://www.wowhead.com/item=153204/all-seers-eye",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 39,
+      itemName: "Baarut the Brisk",
+      itemUrl: "https://www.wowhead.com/item=153193/baarut-the-brisk",
+      category: "Toy",
+      bronzeCost: 10000
+   },
+   {
+      id: 40,
+      itemName: "Barrier Generator",
+      itemUrl: "https://www.wowhead.com/item=153183/barrier-generator",
+      category: "Toy",
+      bronzeCost: 80000
+   },
+   {
+      id: 41,
+      itemName: "Spire of Spite",
+      itemUrl: "https://www.wowhead.com/item=153124/spire-of-spite",
+      category: "Toy",
+      bronzeCost: 35000
+   },
+   {
+      id: 42,
+      itemName: "Sightless Eye",
+      itemUrl: "https://www.wowhead.com/item=153293/sightless-eye",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 43,
+      itemName: "Blue Conservatory Scroll",
+      itemUrl: "https://www.wowhead.com/item=153179/blue-conservatory-scroll",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 44,
+      itemName: "Red Conservatory Scroll",
+      itemUrl: "https://www.wowhead.com/item=153181/red-conservatory-scroll",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 45,
+      itemName: "Yellow Conservatory Scroll",
+      itemUrl: "https://www.wowhead.com/item=153180/yellow-conservatory-scroll",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 46,
+      itemName: "S.F.E Interceptor",
+      itemUrl: "https://www.wowhead.com/item=153253/s-f-e-interceptor",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 47,
+      itemName: "Holy Lightsphere",
+      itemUrl: "https://www.wowhead.com/item=153182/holy-lightsphere",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 48,
+      itemName: "Micro-Artillery Controller",
+      itemUrl: "https://www.wowhead.com/item=153126/micro-artillery-controller",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 49,
+      itemName: "Vixx's Chest of Tricks",
+      itemUrl: "https://www.wowhead.com/item=152982/vixxs-chest-of-tricks",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 50,
+      itemName: "Unstable Portal Emitter",
+      itemUrl: "https://www.wowhead.com/item=153004/unstable-portal-emitter",
+      category: "Toy",
+      bronzeCost: 25000
+   },
+   {
+      id: 51,
+      itemName: "Legion Communication Orb",
+      itemUrl: "https://www.wowhead.com/item=153194/legion-communication-orb",
+      category: "Toy",
+      bronzeCost: 25000
+   }
+];
