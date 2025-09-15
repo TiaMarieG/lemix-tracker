54420fabea1f028f4d5dd56592f8f9b3d00ebbc4
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-09-15 11:05:05 -0700

    Added a ts file for Arturos Vendor Data

---
+++ src/data/ArturosVendorData.ts
@@ -0,0 +1,129 @@
+export interface VendorInfo {
+   id: number;
+   itemName: string;
+   itemUrl: string;
+   category: "Ensemble";
+   bronzeCost: number;
+}
+
+export const ArturosVendorData: VendorInfo[] = [
+   {
+      id: 1,
+      itemName: "Ensemble: Seawitch's Terrorcloth",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 2,
+      itemName: "Ensemble: Nighthide Coat",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 3,
+      itemName: "Ensemble: Chains of Nightmare's Embrace",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 4,
+      itemName: "Ensemble: Suramar Silver Plating",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 5,
+      itemName: "Ensemble: Vesture of Borrowed Souls",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 6,
+      itemName: "Ensemble: Sanguine Oath Vestments",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 7,
+      itemName: "Ensemble: Bindings of Hungering Flesh",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 8,
+      itemName: "Ensemble: Thirsting Hides",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 9,
+      itemName: "Ensemble: Armor of the Skyfather's Chosen",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 10,
+      itemName: "Ensemble: Ravensteel Mail",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 11,
+      itemName: "Ensemble: Honorforged Valorplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 12,
+      itemName: "Ensemble: Bloodforged Battleplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 13,
+      itemName: "Ensemble: Nightforged Felplate",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 2,
+      itemName: "Ensemble: Nighthide Coat",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 14,
+      itemName: "Ensemble: Raiment of Night Eternal",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 15,
+      itemName: "Ensemble: Guise of the Nightstalker",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   },
+   {
+      id: 16,
+      itemName: "Ensemble: Scalemail of Devouring Night",
+      itemUrl: "N/A",
+      category: "Ensemble",
+      bronzeCost: 15000
+   }
+]
